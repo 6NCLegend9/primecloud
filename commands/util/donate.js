eb6780f929361f804ff86d26bf26a0786f7560ca
6NCLegend9 <59505853+6NCLegend9@users.noreply.github.com>
2020-05-31 22:00:34 +0300

    Create donate.js

---
+++ commands/util/donate.js
@@ -0,0 +1,34 @@
+const { Command } = require('discord.js-commando');
+const { MessageEmbed, version: djsVersion } = require('discord.js');
+const moment = require('moment');
+require('moment-duration-format');
+const { formatNumber, embedURL } = require('../../util/Util');
+
+module.exports = class DonateCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'donate',
+			aliases: ['paypal'],
+			group: 'util',
+			memberName: 'donate',
+			description: 'Responds with the bot\'s donation links.',
+			guarded: true,
+			credit: [
+				{
+					name: 'PayPal',
+					url: 'https://www.paypal.com/us/home',
+					reason: 'Donation Gathering'
+				}
+			]
+		});
+	}
+
+	async run(msg) {
+    
+		const embed = new MessageEmbed()
+			.setColor("#ff3232")
+		.setTitle('donation')		
+    .addField('❯ Contribute to development!', embedURL('donate', 'https://www.paypal.me/saradalmeida'), true)
+		return msg.embed(embed);
+	}
+};
