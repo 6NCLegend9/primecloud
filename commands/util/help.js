14eb0dbae07eb69de485d6f1bc6044616860eff3
6NCLegend9 <59505853+6NCLegend9@users.noreply.github.com>
2020-05-31 22:00:12 +0300

    Create help.js

---
+++ commands/util/help.js
@@ -0,0 +1,69 @@
+const { MessageEmbed } = require('discord.js');
+const { Command } = require('discord.js-commando');
+const { stripIndents } = require('common-tags');
+
+module.exports = class helpCommand extends Command {
+constructor(client) {
+	super(client, {
+			name: 'help',
+			aliases: ['commands', 'command-list'],
+			group: 'util',
+			memberName: 'help',
+			description: 'Displays a list of available commands, or detailed information for a specific command.',
+			guarded: true,
+		args: [
+				{
+					key: 'command',
+					prompt: 'Which command would you like to view the help for?',
+					type: 'command',
+					default: ''
+			}
+			]
+	});
+	}
+
+	async run(msg, { command }) {
+		if (!command) {
+			const embed = new MessageEmbed()
+				.setTitle('Command List')
+				.setColor("#ff3232");
+				let cmdCount = 0;
+			for (const group of this.client.registry.groups.values()) {
+				const owner = this.client.isOwner(msg.author);
+				const commands = group.commands.filter(cmd => {
+					if (owner) return true;
+					if (cmd.ownerOnly || cmd.hidden) return false;
+					return true;
+				});
+				if (!commands.size) continue;
+				cmdCount += commands.size;
+				embed.addField(
+					`❯ ${group.name}`,
+					commands.map(cmd => `\`${cmd.name}\``).join(', ')
+				);
+			}
+			if (cmdCount === this.client.registry.commands.size) {
+				embed.setFooter(`${this.client.registry.commands.size} Commands`);
+			} else {
+				embed.setFooter(`${this.client.registry.commands.size} Commands (${cmdCount} Shown)`);
+			}
+			try {
+				const msgs = [];
+				msgs.push(await msg.direct({ embed }));
+				if (msg.channel.type !== 'dm') msgs.push(await msg.say('📬 Sent you a DM with information.'));
+				return msgs;
+			} catch {
+				return msg.reply('Failed to send DM. You probably have DMs disabled.');
+			}
+		}
+
+		return msg.say(stripIndents`
+			__Command **${command.name}**__${command.guildOnly ? ' (Usable only in servers)' : ''}
+			${command.description}${command.details ? `\n${command.details}` : ''}
+
+			**Format:** ${msg.anyUsage(`${command.name} ${command.format || ''}`)}
+			**Aliases:** ${command.aliases.join(', ') || 'None'}
+			**Group:** ${command.group.name} (\`${command.groupID}:${command.memberName}\`)
+		`);
+	}
+};
