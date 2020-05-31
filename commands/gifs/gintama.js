cddb1c12a8d11056fe4360bba2f18c61451f5aab
6NCLegend9 <59505853+6NCLegend9@users.noreply.github.com>
2020-05-31 22:32:05 +0300

    Create gintama.js

---
+++ commands/gifs/gintama.js
@@ -0,0 +1,29 @@
+const fetch = require('node-fetch');
+const { tenorAPI } = require('../../config.json');
+const { Command } = require('discord.js-commando');
+
+module.exports = class GintamaCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'gintama',
+      group: 'gifs',
+      memberName: 'gintama',
+      description: 'Replies with a gintama gif!',
+      throttling: {
+        usages: 2,
+        duration: 8
+      }
+    });
+  }
+
+  run(message) {
+      
+    fetch(`https://api.tenor.com/v1/random?key=${tenorAPI}&q=gintama&limit=1`)
+      .then(res => res.json())
+      .then(json => message.say(json.results[0].url))
+      .catch(e => {
+        message.say('Failed to find a gif :slight_frown:');
+        console.error(e);
+        return;
+      });
+  }
