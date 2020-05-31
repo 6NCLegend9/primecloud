63b77677905aaab4265441198306a4389e1c5221
6NCLegend9 <59505853+6NCLegend9@users.noreply.github.com>
2020-05-31 22:03:42 +0300

    Create dec-talk.js

---
+++ commands/other/dec-talk.js
@@ -0,0 +1,66 @@
+const { Command } = require('discord.js-commando');
+const request = require('node-superfetch');
+
+module.exports = class DECTalkCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'dec-talk',
+			aliases: ['text-to-speech', 'tts'],
+			group: 'other',
+			memberName: 'dec-talk',
+			description: 'The world\'s best Text-to-Speech.',
+			throttling: {
+				usages: 1,
+				duration: 10
+			},
+			userPermissions: ['CONNECT', 'SPEAK'],
+			credit: [
+				{
+					name: 'calzoneman',
+					url: 'https://github.com/calzoneman',
+					reason: 'API',
+					reasonURL: 'https://github.com/calzoneman/aeiou'
+				},
+				{
+					name: 'Digital Equipment Corporation',
+					url: 'http://gordonbell.azurewebsites.net/digital/timeline/tmlnhome.htm',
+					reason: 'Original DECTalk Software'
+				},
+				{
+					name: 'NASA',
+					url: 'https://www.nasa.gov/',
+					reason: 'Original "Moonbase Alpha" Game',
+					reasonURL: 'https://store.steampowered.com/app/39000/Moonbase_Alpha/'
+				}
+			],
+			args: [
+				{
+					key: 'text',
+					prompt: 'What text do you want to say?',
+					type: 'string',
+					max: 1024
+				}
+			]
+		});
+	}
+
+	async run(msg, { text }) {
+		const inGuild = msg.guild ? undefined : null;
+		const connection = this.client.voice.connections.get(msg.guild.id);
+		if (!connection) {
+			return msg.reply(`I am not in a voice channel. Use ${msg.anyUsage('join', inGuild, inGuild)} to fix that!`);
+		}
+		try {
+			const { url } = await request
+				.get('http://tts.cyzon.us/tts')
+				.query({ text });
+			connection.play(url);
+			if (msg.channel.permissionsFor(this.client.user).has(['ADD_REACTIONS', 'READ_MESSAGE_HISTORY'])) {
+				await msg.react('🔉');
+			}
+			return null;
+		} catch (err) {
+			return msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
+		}
+	}
+};
