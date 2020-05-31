29700e9ea10d5e62a852ac20bf4dd13379de6039
6NCLegend9 <59505853+6NCLegend9@users.noreply.github.com>
2020-05-31 22:48:21 +0300

    Create resume.js

---
+++ commands/music/resume.js
@@ -0,0 +1,29 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class ResumeCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'resume',
+      aliases: ['resume-song', 'continue'],
+      memberName: 'resume',
+      group: 'music',
+      description: 'Resume the current paused song',
+    });
+  }
+
+  run(message) {
+    var voiceChannel = message.member.voice.channel;
+    if (!voiceChannel) return message.reply('Join a channel and try again');
+
+    if (
+      typeof message.guild.musicData.songDispatcher == 'undefined' ||
+      message.guild.musicData.songDispatcher === null
+    ) {
+      return message.reply('There is no song playing right now!');
+    }
+
+    message.say('Song resumed <:circledplay:713741939433799701>');
+
+    message.guild.musicData.songDispatcher.resume();
+  }
+};
