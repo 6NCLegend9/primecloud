47bbf8f1516c24a5d6bca7581aa7c871b0fbd309
6NCLegend9 <59505853+6NCLegend9@users.noreply.github.com>
2020-05-31 22:06:38 +0300

    Create meme.js

---
+++ commands/other/meme.js
@@ -0,0 +1,45 @@
+const { Command } = require('discord.js-commando');
+const Discord = require('discord.js');
+const randomPuppy = require('random-puppy');
+const subreddits = [
+    "memes",
+    "DeepFriedMemes",
+    "bonehurtingjuice",
+    "surrealmemes",
+    "dankmemes",
+    "meirl",
+    "me_irl",
+    "funny"
+]
+ 
+module.exports = class MemeCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'meme',
+            group: 'other',
+            memberName: 'meme',
+           
+            description: 'Sends a random meme from selected subreddits!',
+            examples: ['~meme'],
+            details: "There is no NSFW filter on this! \n\If there is an NSFW meme, please remove it by reacting with a '🎴' emoji!",
+            throttling: {
+                usages: 2,
+                duration: 10
+            }
+        });
+    }
+
+    run(message) {
+        var randSubreddit = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
+
+        randomPuppy(randSubreddit)
+            .then(url => {
+                const embed = new Discord.MessageEmbed()
+                    .setFooter(`${randSubreddit}`)
+                    .setDescription(`[MEME REVIEW](${url})`)
+                    .setImage(url)
+                    .setColor('#ff3232');
+                return message.channel.send({ embed });
+            })
+    }
+}
