4900fc507ab4d45fe3bf12db4a82818cc25cd696
6NCLegend9 <59505853+6NCLegend9@users.noreply.github.com>
2020-05-31 22:46:57 +0300

    Create pause.js

---
+++ commands/music/pause.js
@@ -0,0 +1,29 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class PauseCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'pause',
+      aliases: ['pause-song', 'hold', 'stop'],
+      memberName: 'pause',
+      group: 'music',
+      description: 'Pause the current playing song',
+    });
+  }
+
+  run(message) {
+    var voiceChannel = message.member.voice.channel;
+    if (!voiceChannel) return message.reply('Join a channel and try again');
+
+    if (
+      typeof message.guild.musicData.songDispatcher == 'undefined' ||
+      message.guild.musicData.songDispatcher == null
+    ) {
+      return message.say('There is no song playing right now!');
+    }
+
+    message.say('Song paused <:pause:713742415080718337>');
+
+    message.guild.musicData.songDispatcher.pause();
+  }
+};
