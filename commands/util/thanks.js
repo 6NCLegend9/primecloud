9d42bc797be0c160c4e9f6dd33c00008435954f4
6NCLegend9 <59505853+6NCLegend9@users.noreply.github.com>
2020-07-15 14:38:24 +0300

    Update thanks.js

--- commands/util/thanks.js
+++ commands/util/thanks.js
@@ -16,7 +16,7 @@ module.exports = class ServerCommand extends Command {
 	async run(msg) {
 		const embed = new MessageEmbed()
 			 embed.setTitle("<:BlueVerifiedBadge:709062879738855584> Special Thanks")
-      .setColor("ff3232")                                                     
+      .setColor("BA55D3")                                                     
       .addField("Main Developers", "Skies & NC_Legend.\nModmail Developer - -Kat-. \nCosmic Bot Developer - Brendan_. \nCATO - Our Developer.")
       .addField("<:BlueVerifiedBadge:709062879738855584> Verified Bots", "Cloud Music & Cloud Giveaways")
       .addField("Special Thanks to everyone", "Special thanks to everyone that invited our bot to your server. Skies & I have been working on this project for a bit now. After discord released their <:BlueVerifiedBadge:709062879738855584> badge for bot Developers I've been excited because of what we can do in the near future with our bot. Cloud Music and Cloud Giveaway bots both got <:BlueVerifiedBadge:709062879738855584>. This has been a long journey but we're here to strive to success and make our bot as successful as possible. we love every single one of our users that use our bot! Thank you for sticking with us. we will keep improving our bot and upgrade our Hosting so its better for our users!!")
