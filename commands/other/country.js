96e007421960a52a93d8bd229453d88a09cd5d94
6NCLegend9 <59505853+6NCLegend9@users.noreply.github.com>
2020-07-15 14:28:57 +0300

    Update country.js

--- commands/other/country.js
+++ commands/other/country.js
@@ -34,7 +34,7 @@ module.exports = class CountryCommand extends Command {
 			const { body } = await request.get(`https://restcountries.eu/rest/v2/name/${query}`);
 			const data = body[0];
 			const embed = new MessageEmbed()
-				.setColor("#ff3232")
+				.setColor("#BA55D3")
 				.setTitle(data.name)
 				.setThumbnail(`https://www.countryflags.io/${data.alpha2Code}/flat/64.png`)
 				.addField('❯ Population', formatNumber(data.population), true)
