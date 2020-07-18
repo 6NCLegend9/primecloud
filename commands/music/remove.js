6e961f5401ca8db66bb7ebf225596cccb6031310
6NCLegend9 <59505853+6NCLegend9@users.noreply.github.com>
2020-07-18 20:54:10 +0300

    Update remove.js

--- commands/music/remove.js
+++ commands/music/remove.js
@@ -36,7 +36,8 @@ module.exports = class RemoveSongCommand extends Command {
    // return message.say(`<:trashcan:713747946608525383> Removed song number ${songNumber} from queue`);
     const embed = new MessageEmbed()
       .setColor('#BA55D3')
-    	.addField('Removed', `<:trashcan:713747946608525383> Removed song number ${songNumber} from queue`)
+ 	  	.addField('Removed', `Removed song number ${songNumber} from queue`)
+   // .addField('Removed', `<:trashcan:713747946608525383> Removed song number ${songNumber} from queue`)
     	return message.embed(embed);
   }
 };
