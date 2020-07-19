a3664722ec717280f142d1f44d7d2bb59903969b
6NCLegend9 <59505853+6NCLegend9@users.noreply.github.com>
2020-07-19 20:27:35 +0300

    Update skip.js

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -13,7 +13,7 @@ module.exports = class SkipCommand extends Command {
 
   run(message) {
     const voiceChannel = message.member.voice.channel;
-    if (!voiceChannel) return message.reply('You cannot use the command unless youre in the same channel as Cloud Music!');
+    if (!voiceChannel) return message.reply('You cannot use the command unless youre in the same channel as Cloud Music Premium!');
 
     if (
       typeof message.guild.musicData.songDispatcher == 'undefined' ||
