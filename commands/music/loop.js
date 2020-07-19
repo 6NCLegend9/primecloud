95243fbe62ab2c10083717a17efeec731f18e372
6NCLegend9 <59505853+6NCLegend9@users.noreply.github.com>
2020-07-19 19:37:42 +0300

    Update loop.js

--- commands/music/loop.js
+++ commands/music/loop.js
@@ -37,7 +37,7 @@ module.exports = class LoopCommand extends Command {
     // prettier-ignore
     const embed = new MessageEmbed()  
    .setColor('#BA55D3')
-     	.addField('Disconnected', `:arrows_counterclockwise: ${message.guild.musicData.nowPlaying.title} looped ${numOfTimesToLoop} ${
+     	.addField('Looping', `<:purple_loop:734445630956634213> ${message.guild.musicData.nowPlaying.title} looped ${numOfTimesToLoop} ${
         (numOfTimesToLoop == 1) ? 'time' : 'times'}`)
     //.addField('Disconnected', `<:repeat:713742643489800263> ${message.guild.musicData.nowPlaying.title} looped ${numOfTimesToLoop} ${   (numOfTimesToLoop == 1) ? 'time' : 'times'}`)
     	return message.embed(embed);
