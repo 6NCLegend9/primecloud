d1138b4f30cdfc63a2182bce58770db526980295
6NCLegend9 <59505853+6NCLegend9@users.noreply.github.com>
2020-07-19 19:33:54 +0300

    Create invite.js

---
+++ commands/util/invite.js
@@ -0,0 +1,29 @@
+  
+const { Command } = require('discord.js-commando');
+const { MessageEmbed, version: djsVersion } = require('discord.js');
+const moment = require('moment');
+require('moment-duration-format');
+const { formatNumber, embedURL } = require('../../util/Util');
+
+module.exports = class InviteCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'invite',
+			aliases: ['inv'],
+			group: 'util',
+			memberName: 'invite',
+			description: 'prime link invation.',
+			hidden: true,
+			guarded: true
+		});
+	}
+
+	async run(msg) {
+    
+		const embed = new MessageEmbed()
+			.setColor("#BA55D3")
+		.setTitle('invite')		
+    .addField('prime invite', embedURL('invite', 'https://discord.com/api/oauth2/authorize?client_id=662988653299236874&permissions=37091655&scope=bot'), true)
+		return msg.embed(embed);
+	}
+};
